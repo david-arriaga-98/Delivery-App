fe72c49ab0c6d05dc6f35c000d74db13b8ee82c0
David Arriaga <brayan.arriaga@ug.edu.ec>
2020-06-09 10:35:46 -0500

    Agrgar contenido al modulo de envios

--- src/components/Shipping.jsx
+++ src/components/Shipping.jsx
@@ -3,8 +3,37 @@ import { Row, Col } from 'react-bootstrap';
 
 export default () => {
 	return (
-		<Row className="justify-content-center">
-			<Col md="8">Aqui va el componente</Col>
+		<Row className="justify-content-center border-dark">
+			<Col md="11">
+				<Row>
+					<Col md="3">Imagen</Col>
+					<Col md="9">
+						<Row>
+							<Col md="4">01/06/2020</Col>
+							<Col md="4">
+								Hora de Entrega: 02:00 pm
+							</Col>
+							<Col md="4">Distancia: 14.2 Km</Col>
+						</Row>
+						<Row>
+							<Col md="12">
+								<h6>
+									De: San Cristobal, Zona 8 Mixco,
+									Guatemala
+								</h6>
+							</Col>
+						</Row>
+						<Row>
+							<Col md="12">
+								<h6>
+									De: San Cristobal, Zona 8 Mixco,
+									Guatemala
+								</h6>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+			</Col>
 		</Row>
 	);
 };
